02d3b94ecf18e55f51ee89d4a1d34fe62521cc75
Abdul Jaweed <jdgaming7320@gmail.com>
2023-03-28 05:10:09 +0530

    data transformation config entity and artifact entity added

---
+++ heart/components/data_transformation.py
@@ -0,0 +1,12 @@
+# Filling Missing Values
+# Handling Outlier
+# Imbalanced Data Handling
+# Convert catergorical to numerical 
+
+
+
+
+
+
+class DataTransformation:
+    pass
--- heart/entity/artifact_entity.py
+++ heart/entity/artifact_entity.py
@@ -13,3 +13,10 @@ class DataValidationArtifact:
     report_file_path:str
     
     
+
+@dataclass
+class DataTransformationArtifact:
+    transform_object_path:str
+    transformed_train_path:str
+    transformed_test_path:str
+    target_encoder_path:str
